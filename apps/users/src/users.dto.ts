d5206f1065c8de6a2fc9c1ff31defd7bdadd6d88
zutin <gianluccaz@hotmail.com>
2023-10-24 22:23:20 -0300

    chore: users entity

---
+++ apps/users/src/users.dto.ts
@@ -0,0 +1,15 @@
+export type UserDTO = {
+  id?: string
+  firstName: string
+  lastName: string
+  username: string
+  email: string
+  dob?: Date | string
+  status?: string
+  pfp?: string
+  createdAt?: Date | string
+  updatedAt?: Date | string
+  deletedAt?: Date | string
+  allowPublicName?: boolean
+  allowPublicDob?: boolean
+}
